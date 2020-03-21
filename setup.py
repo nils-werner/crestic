e2428abdd48e5c36c6e6b92b837e08eeb0298424
Nils Werner <nils.werner@gmail.com>
2020-03-21 15:13:51 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='crestic',
-    version='0.3.0a1',
+    version='0.3.0',
     description='Configurable restic',
     author='Nils Werner',
     author_email='nils.werner@gmail.com',
