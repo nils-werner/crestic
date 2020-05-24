e570e65bc4bea00def87f7b16011803a6612c5cf
Nils Werner <nils.werner@gmail.com>
2020-05-24 10:51:47 +0200

    Replaced setup.py with setup.cfg

--- setup.py
+++ setup.py
@@ -1,29 +1,3 @@
-#!/usr/bin/env python
+from setuptools import setup
 
-from setuptools import setup, find_packages
-
-setup(
-    name='crestic',
-    version='0.4.1',
-    description='Configurable restic',
-    author='Nils Werner',
-    author_email='nils.werner@gmail.com',
-    py_modules=['crestic'],
-    entry_points={
-        'console_scripts': [
-            'crestic = crestic:cli',
-        ]
-    },
-    extras_require={
-        'tests': [
-            'pytest',
-            'pytest-cov',
-            'pytest-mock',
-            'pytest-pycodestyle',
-            'appdirs',
-        ],
-        'appdirs': [
-            'appdirs',
-        ],
-    },
-)
+setup()
